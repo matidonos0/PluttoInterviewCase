a1303e17d240c67d8ef6ec4ecc0f8a4f07759abc
matidonos0 <84134331+matidonos0@users.noreply.github.com>
2023-06-12 11:05:25 +0000

    v1, working fine

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import plotly.express as px
 import json
 import random
 
-
 from flask import Flask, render_template, request
 from plotly.graph_objs import Bar, Pie
 from risk_classifier import riskyfier
@@ -19,6 +18,7 @@ ids_count = data['ID'].nunique()
 companies_count = data[data['Tipo'] == 'Empresa']['ID'].count()
 persons_count = data[data['Tipo'] == 'Persona']['ID'].count()
 risky_pct = round((data[data['risk'] == 'high_risk']['ID'].nunique()/ids_count)*100,2)
+rut = list(data.RUT)
 
 
 app = Flask(
@@ -31,16 +31,19 @@ app = Flask(
 @app.route('/')
 def index():
   company_pct = round(companies_count/ids_count*100,2)
+  industries = list(data['Industria SII'])
   
+  #graph building
   graphs = [
     px.bar(
-      data.groupby(['risk','Tipo']).count().reset_index(), x='Tipo', y='ID', color='risk', labels={'ID':'Clients'}, color_discrete_map = {'high_risk': '#F7C0BB', 'medium_risk': '#30BFDD' , 'low_risk': '#7FD4C1'}
+      data.groupby(['risk','Tipo']).count().reset_index(), x='Tipo', y='ID', color='risk', labels={'ID':'Clients'}, color_discrete_map = {'high_risk': '#F7C0BB', 'medium_risk': '#30BFDD' , 'low_risk': '#7FD4C1'},
+      title='Clients by Type and Risk'
     ),
     px.pie(
-      data.groupby('Tipo').count().reset_index(), values='ID', names='Tipo'
+      data.groupby('Tipo').count().reset_index(), values='ID', names='Tipo', labels={'ID':'Clients'}, title='Client Distribution'
     ),
     px.sunburst(
-      data.groupby(['risk','Tipo']).count().reset_index(), path=['Tipo', 'risk'], values='ID'
+      data.groupby(['risk','Tipo']).count().reset_index(), path=['Tipo', 'risk'], values='ID', title='Total Portfolio Distribution', labels={'ID':'Clients', 'labels':'Risk', 'parent': 'Tipo Cliente'}
     )
   ]
   
@@ -57,23 +60,26 @@ def index():
                          risky_pct = risky_pct
   )
 
-
 # web page that handles user query and displays model results
 @app.route('/detail')
 def detail():
+  try:
   # save user input in query
-  query = request.args.get('query', '')
-  result = data[data['RUT'] == int(query)]
-  return render_template(
-    'detail.html',
-    query=query,
-    result = result,
-    name = result['Nombre'],
-    risk = result['risk']
-  )
+    query = request.args.get('query', '')
+    result = data[data['RUT'] == int(query)]
+    return render_template(
+      'detail.html',
+      query=query,
+      result = result,
+      name = result['Nombre'],
+      risk = result['risk']
+    )
+  except:
+    return render_template('index.html',
+                          additional_text = 'Wrong input, please insert a valid one')
 
 @app.route('/industry')
-def industy():
+def industry():
   df_risks = data.groupby(['Industria SII', 'risk'])['ID'].count().reset_index().sort_values(by='ID', ascending=False)
   #this gets top risk companies
   df_high_risk = df_risks[df_risks['risk'] == 'high_risk'].head(3)
@@ -81,15 +87,32 @@ def industy():
   #this is a filter to be used to get data_X_risk
   high_risk_industries = list(df_high_risk['Industria SII']) 
   medium_risk_industries = list(df_medium_risk['Industria SII'])
+
+  #build risk kpis
+  data['boletin'] = data['Tiene B. comercial'] + data['Tiene B. laboral']
+  data['debt_late'] = data['Deuda Directa morosa'] + data['Deuda Leasing morosa']
+  data['written_off'] = data['Deuda Indirecta castigada'] + data['Deuda Directa castigada']
+  
   #this gets KPIs for the indrustries at given risk
-  data_medium_risk = data.query('`Industria SII` in @medium_risk_industries').groupby(['Industria SII'])[['Deuda Directa castigada', 'Deuda Indirecta castigada', 'Tiene B. concursal']].mean()
-  data_high_risk = data.query('`Industria SII` in @high_risk_industries').groupby(['Industria SII'])[['Deuda Directa castigada', 'Deuda Indirecta castigada', 'Tiene B. concursal']].mean()
+  data_medium_risk = data.query('`Industria SII` in @medium_risk_industries').groupby(['Industria SII']).agg(
+     avg_debt = ('debt_late', 'mean'),
+     avg_boletin = ('boletin', 'mean'),
+     avg_legal_causes = ('Causas legales en contra','sum'),
+     avg_employees = ('Empleados', 'mean'),
+     count = ('ID','count')).round(2)
+
+  data_high_risk = data.query('`Industria SII` in @high_risk_industries').groupby(['Industria SII']).agg(
+     avg_wod = ('written_off', 'mean'),
+     sum_bankrupt = ('Tiene B. concursal','sum'),
+     avg_employees = ('Empleados', 'mean'),
+     count = ('ID','count')).round(2)
+  data_medium_risk.columns = ['Avg.Written Off Debt', 'Avg.Bulletin Aparitions', 'Avg.Legal Causes Against', 'Avg.Employee Count', 'Clients Count']
+  data_high_risk.columns = ['Avg.Written Off Debt', 'Total Concursal Bulletin Aparitions', 'Avg.Employee Count', 'Clients Count']
+  
   return render_template(
     'industry.html',
-    hr_industries = high_risk_industries,
-    dhr_bankrupty = data_high_risk['Deuda Directa castigada'],
-    tables_hr=[data_high_risk.to_html(classes='data', header=True)],
-    tables_mr=[data_medium_risk.to_html(classes='data')]
+    tables_hr=[data_high_risk.reset_index().to_html(classes='table', header=True, index=False)],
+    tables_mr=[data_medium_risk.reset_index().to_html(classes='table',header=True, index=False)]
   ) 
 
 if __name__ == "__main__":
